c14719732848bea052311d1bd2952efb2b054cd1
Phatvnzoon <luuthanhphat2006@gmail.com>
2025-10-24 17:32:58 +0700

    Them cau C

--- project.cpp
+++ project.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include "mylib.h"
 
 const int MAX_DAUSACH = 10000;
 
@@ -193,6 +194,91 @@ void xoathe(TreeDocGia& a, int x ){
        return ;
     }
 };
+
+//Câu C
+//Đánh mã sách tự động
+string randomMaSach(){ // tạo mã sách có 6 chữ số
+    string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    string result;
+    while(1){
+        result = "";
+        bool fullLetter = false, fullDigit = false;
+        for (int i = 0; i < 6; i++) {
+            char c = chars[rand() % chars.size()];
+            result += c; 
+            if (isalpha(c)) fullLetter = true;
+            if (isdigit(c)) fullDigit = true;
+        }
+        if (fullLetter && fullDigit) break; //Bỏ trường hợp full chữ hoặc full số
+    }   
+    
+    return result;
+}
+
+bool MaSachTrung(DauSach *ds, string &Ma){
+    SACH p = ds->FirstSach;
+    while (p != NULL) {
+        if (strcmp(p->data.MASACH, Ma.c_str()) == 0)
+            return true;
+        p = p->next;
+    }
+    return false;
+}
+
+void NhapDauSach(DS_DAUSACH &ds_dausach){
+    if (ds_dausach.n >= MAX_DAUSACH){
+        cout << "Danh sach da day, khong the nhap!";
+        return;
+    }
+
+    DauSach *p = new DauSach();
+    NhapMa("Nhap ISBN(13 so): ", p->ISBN, 15);
+    NhapChuoi("Nhap ten sach: ", p->TENSACH,101);
+    p->SOTRANG = NhapSo("Nhap so trang: ");
+    NhapChuoi("Nhap tac gia: ", p->TACGIA, 51);
+    p->NAMXUATBAN = NhapSo("Nhap Nam xuat ban: ");
+    NhapChuoi("Nhap the loai: ", p->THELOAI, 31);
+    int slsach = NhapSo("Nhap so luong sach: ");
+
+    for(int i = 0; i < slsach; i++){
+        SACH newSach = new nodeSach();
+        
+        // Tạo mã sách tự động và kiểm tra trùng
+        string maSach;
+        do {
+            maSach = randomMaSach();
+        } while(MaSachTrung(p, maSach));
+        
+        strcpy(newSach->data.MASACH, maSach.c_str());
+        newSach->data.trangthai = 0; // 0: trong kho
+        NhapChuoi("Nhap vi tri: ", newSach->data.vitri, 51);
+        
+        // Thêm vào đầu danh sách liên kết
+        newSach->next = p->FirstSach;
+        p->FirstSach = newSach;
+        
+        cout << "Da tao sach voi ma: " << maSach << endl;
+    }
+
+    int viTriChen = ds_dausach.n;
+    for(int i = 0; i < ds_dausach.n; i++){
+        if(strcmp(p->TENSACH, ds_dausach.nodes[i]->TENSACH) < 0){
+            viTriChen = i;
+            break;
+        }
+    }
+    
+    // Dịch chuyển các phần tử để chèn vào đúng vị trí
+    for(int i = ds_dausach.n; i > viTriChen; i--){
+        ds_dausach.nodes[i] = ds_dausach.nodes[i-1];
+    }
+
+    ds_dausach.nodes[ds_dausach.n] = p; // thêm phần tử vào danh sách
+    ds_dausach.n++;
+
+    cout << "\nDa them thanh cong!" << endl;
+}
+
 int main() {
     DS_DAUSACH dsdausach;
     TreeDocGia dsdocgia = NULL;
