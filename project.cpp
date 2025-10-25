eb811d27f3c12aa4a9508707a74d467bde2f4501
bebest04 <minhdj20061907@gmail.com>
2025-10-25 16:49:02 +0700

    muonsach-chuatest

--- project.cpp
+++ project.cpp
@@ -6,7 +6,7 @@ const int MAX_DAUSACH = 10000;
 
 struct Sach {
     char MASACH[16];
-    int trangthai;
+    int trangthai; // =0 dc muon , =1 đã mượn, =2 đã thanh lý
     char vitri[51];
 };
 
@@ -24,6 +24,7 @@ struct DauSach {
     int NAMXUATBAN;
     char THELOAI[31];
     SACH FirstSach = NULL;
+    int slm = 0;
 };
 
 struct DS_DAUSACH {
@@ -41,7 +42,7 @@ struct MuonTra {
     char MASACH[16];
     char NgayMuon[11];
     char NgayTra[11];
-    int trangthai;
+    int trangthai2; // =0 đang mượn , =1 đã trả , =2 là mất sách
 };
 
 struct nodeMuonTra {
@@ -49,7 +50,11 @@ struct nodeMuonTra {
     nodeMuonTra *next;
 };
 typedef nodeMuonTra* MT;
-
+MT makeMT(){
+    MT a;
+    a->next = NULL;
+    return a;
+};
 struct TheDocGia {
     int MATHE=0;
     int sum=0;
@@ -153,7 +158,7 @@ void xoanode2la(TreeDocGia& a,TreeDocGia &p){
     }
 };
 void xoathe(TreeDocGia& a, int x ){   // xóa thẻ trong cây
-    if(a==NULL){
+        if(a==NULL){
         return;
     }
         xoathe(a->left,x); 
@@ -171,8 +176,8 @@ void xoathe(TreeDocGia& a, int x ){   // xóa thẻ trong cây
             delete p;
         }
         else if(a->right == NULL&& a->left == NULL){
-                delete a;
-                a = nullptr;
+            delete a;
+            a = nullptr;
            }
         else {
             TreeDocGia p = a;
@@ -197,7 +202,7 @@ void xoathe(TreeDocGia& a, int x ){   // xóa thẻ trong cây
 void indsmuontra(MT & a){   // in ds mượn trả
     MT p = a;
     while(p == NULL){
-        cout << p->mt.MASACH <<" "<< p->mt.NgayMuon<<" "<<p->mt.NgayTra<<" "<<p->mt.trangthai<<endl;
+        cout << p->mt.MASACH <<" "<< p->mt.NgayMuon<<" "<<p->mt.NgayTra<<" "<<p->mt.trangthai2<<endl;
         p = p->next;
     }
 };
@@ -215,7 +220,126 @@ void checkdsmuonsach(TreeDocGia & a, int x ){  // in danh sách mượn trả
         indsmuontra(a->dg.dsmuontra);
     }   
 };
+void muonsach(DS_DAUSACH & a, TreeDocGia & b,const char* s,int x){
+    if(b == NULL){
+        return; 
+    }
+    else if(b->dg.MATHE > x){
+        muonsach(a,b->left,s,x);
+    }
+    else if(b->dg.MATHE < x){
+        muonsach(a,b->right,s,x);
+    }
+    else {
+        for(int i = 0 ; i < a.n;++i){
+            if(strcmp(a.nodes[i]->TENSACH,s)==0){ // dò tên sách trùng
+                SACH temp = a.nodes[i]->FirstSach;
+                while(temp == NULL){      
+                    if(temp->data.trangthai == 0){ // dò sách đó có ai mượn chưa
+                        MT tmp = makeMT();
+                        MT p = b->dg.dsmuontra;
+                        if(p == NULL){
+                         strcpy(tmp->mt.MASACH,temp->data.MASACH);
+                         tmp->mt.trangthai2 = 0;
+                         return;
+                        }
+                        else{
+                            while(p->next!=NULL){
+                                b->dg.dsmuontra = b->dg.dsmuontra->next;
+                            }
+                            strcpy(tmp->mt.MASACH,a.nodes[i]->FirstSach->data.MASACH); // đã mượn
+                         tmp->mt.trangthai2 = 0;
+                            p->next = tmp;
+                        }
+                        temp->data.trangthai = 1;
+                        return;
+                    }
+                    temp = temp->next;
+                }
+            }
+        }
+    }
+};
+//Đánh mã sách tự động
+string randomMaSach(){ // tạo mã sách có 6 chữ số
+    string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    string result;
+    while(1){
+        result = "";
+        bool fullLetter = false, fullDigit = false;
+        for (int i = 0; i < 6; i++) {
+            char c = chars[rand() % chars.size()];
+            result += c; 
+            if (isalpha(c)) fullLetter = true;
+            if (isdigit(c)) fullDigit = true;
+        }
+        if (fullLetter && fullDigit) break; //Bỏ trường hợp full chữ hoặc full số
+    }   
+    
+    return result;
+};
+bool MaSachTrung(DauSach *ds, string &Ma){
+    SACH p = ds->FirstSach;
+    while (p != NULL) {
+        if (strcmp(p->data.MASACH, Ma.c_str()) == 0)
+            return true;
+        p = p->next;
+    }
+    return false;
+};
+void NhapDauSach(DS_DAUSACH &ds_dausach){
+    if (ds_dausach.n >= MAX_DAUSACH){
+        cout << "Danh sach da day, khong the nhap!";
+        return;
+    }
 
+    DauSach *p = new DauSach();
+    NhapMa("Nhap ISBN(13 so): ", p->ISBN, 15);
+    NhapChuoi("Nhap ten sach: ", p->TENSACH,101);
+    p->SOTRANG = NhapSo("Nhap so trang: ");
+    NhapChuoi("Nhap tac gia: ", p->TACGIA, 51);
+    p->NAMXUATBAN = NhapSo("Nhap Nam xuat ban: ");
+    NhapChuoi("Nhap the loai: ", p->THELOAI, 31);
+    int slsach = NhapSo("Nhap so luong sach: ");
+
+    for(int i = 0; i < slsach; i++){
+        SACH newSach = new nodeSach();
+        
+        // Tạo mã sách tự động và kiểm tra trùng
+        string maSach;
+        do {
+            maSach = randomMaSach();
+        } while(MaSachTrung(p, maSach));
+        
+        strcpy(newSach->data.MASACH, maSach.c_str());
+        newSach->data.trangthai = 0; // 0: trong kho
+        NhapChuoi("Nhap vi tri: ", newSach->data.vitri, 51);
+        
+        // Thêm vào đầu danh sách liên kết
+        newSach->next = p->FirstSach;
+        p->FirstSach = newSach;
+        
+        cout << "Da tao sach voi ma: " << maSach << endl;
+    }
+
+    int viTriChen = ds_dausach.n;
+    for(int i = 0; i < ds_dausach.n; i++){
+        if(strcmp(p->TENSACH, ds_dausach.nodes[i]->TENSACH) < 0){
+            viTriChen = i;
+            break;
+        }
+    }
+    
+    // Dịch chuyển các phần tử để chèn vào đúng vị trí
+    for(int i = ds_dausach.n; i > viTriChen; i--){
+        ds_dausach.nodes[i] = ds_dausach.nodes[i-1];
+    }
+
+    ds_dausach.nodes[ds_dausach.n] = p; // thêm phần tử vào danh sách
+    ds_dausach.n++;
+
+    cout << "\nDa them thanh cong!" << endl;
+};
 int main() {
     DS_DAUSACH dsdausach;
     TreeDocGia dsdocgia = NULL;
